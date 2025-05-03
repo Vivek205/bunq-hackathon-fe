4ca909c647d42f633fc6dbf2cdc4a2b8eb71a66e
Varun A P <8411309+apvarun@users.noreply.github.com>
2025-05-03 15:20:11 +0200

    button bg

--- components/overlay.tsx
+++ components/overlay.tsx
@@ -14,7 +14,7 @@ export default function Overlay() {
     <div className="fixed inset-0 bg-gray-200 50 z-10 grid place-items-center">
       <div>
         <button
-          className="bg-black text-white py-4 px-8 rounded-full font-bold cursor-pointer min-w-40 "
+          className="bg-black text-white text-xl py-4 px-8 rounded-full font-bold cursor-pointer min-w-40 bg-[url('/button-bg.jpg')] bg-cover bg-center"
           onClick={() => {
             setLoading(true);
             localStorage.setItem("overlay", "true");
