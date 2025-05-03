9355bf78d65ccd35912e2120447673f97a984874
Vivek205 <ichbinvivek@gmail.com>
2025-05-03 14:32:44 +0200

    skeleton style

--- components/ConversationBox/ConversationBoxSkeleton.tsx
+++ components/ConversationBox/ConversationBoxSkeleton.tsx
@@ -4,8 +4,8 @@ import { Skeleton } from "../ui/skeleton";
 export const ConversationBoxSkeleton = () => (
   <Card className={"bg-white mr-4"}>
     <CardContent>
-      <Skeleton className="h-4 w-full rounded-sm bg-sky-100" />
-      <Skeleton className="h-4 w-full mt-2 rounded-sm bg-sky-100" />
+      <Skeleton className="h-4 w-full rounded-sm" />
+      <Skeleton className="h-4 w-full mt-2 rounded-sm" />
     </CardContent>
   </Card>
 );
