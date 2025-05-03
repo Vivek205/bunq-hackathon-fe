eb37810e4a780947262714a2f89568caa17c0b86
Varun A P <8411309+apvarun@users.noreply.github.com>
2025-05-03 15:07:29 +0200

    feat: add dynamic overlay component for Bunq OAuth
    Introduce a new Overlay component to handle Bunq OAuth interactions. The component is dynamically imported to prevent server-side rendering issues. The overlay includes a loading state and persists its closed state in localStorage.

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,16 @@
+"use client";
 import { ChatbotMain } from "@/components/ChatbotMain/ChatbotMain";
+
+import dynamic from "next/dynamic";
+
+
+const Overlay = dynamic(() => import('@/components/overlay'), { ssr: false });
 
 export default function Home() {
   return (
     <div className="flex justify-center h-[calc(100vh-32px)] p-4 border-1 rounded-tl-xl rounded-bl-xl m-4 mr-0 border-r-0 bg-white border-neutral-300">
       <ChatbotMain />
+      <Overlay />
     </div>
   );
 }
